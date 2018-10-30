51393d93ded6f3d1f16c9d9fa31b734d07ed3e67
Kenneth Uyabeme <uyabeme@oa-s231-11.ddi.sheridanc.on.ca>
2018-10-30 12:13:50 -0400

    first commit

--- Lecture OCT 30TH/ViewController.swift
+++ Lecture OCT 30TH/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: outlets
+    @IBOutlet weak var beeAngryslider: UISlider!
+    
+    //MARK: variables
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //print("what's in defaults \n\(defaults.dictionaryRepresentation())")
+        defaults.set("Kenneth", forKey: "name")
+        print("what was in userDefaults was a \(defaults.string(forKey: "name")!)")
     }
-
+    /*When using local storage like defaults there are 2 two you can do
+     
+     put things in User deafaults and get something from it*/
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
