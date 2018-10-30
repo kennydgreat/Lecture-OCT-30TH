72cacefcddff34b3e89b0b20412683cb2f36ff63
Kenneth Uyabeme <uyabeme@oa-s231-11.ddi.sheridanc.on.ca>
2018-10-30 11:46:35 -0400

    Initial Commit

---
+++ Lecture OCT 30TH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lecture OCT 30TH
+//
+//  Created by Kenneth Uyabeme on 10/30/18.
+//  Copyright © 2018 Kenneth Uyabeme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
